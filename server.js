d92547a57dba16bc38e845bdc4195509aa2520b6
adyhariri <mahmoudalhariri1994@gmail.com>
2021-06-22 17:57:52 +0300

    lab13 backend

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const express = require('express')
 const cors=require('cors');
 require('dotenv').config();
 const getBooks=require('./module/Schema')
+
 const app = express();
 app.use(cors());
+app.use(express.json());
 
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 });
-app.get('/book',getBooks);
+app.get('/book',getBooks.getBooks);
+app.post('/books',getBooks.addBook);
+app.post('books/:id',getBooks.deleteBook);
 
 
 
