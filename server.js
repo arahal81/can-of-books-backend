5661830d3fea2636363fe0dcadcead8ec7b83f50
Ali Rahhal <arahal81@gmail.com>
2021-06-22 23:16:07 +0300

    lab13

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const express = require('express')
 const cors=require('cors');
 require('dotenv').config();
-const getBooks=require('./module/Schema')
-
+const myFunctions=require('./module/Schema')
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,9 +11,9 @@ app.use(express.json());
 app.get('/', function (req, res) {
   res.send('Hello World')
 });
-app.get('/book',getBooks.getBooks);
-app.post('/books',getBooks.addBook);
-app.post('books/:id',getBooks.deleteBook);
+app.get('/book',myFunctions.getBooks);
+app.post('/books',myFunctions.addBook);
+app.delete('/books/:id',myFunctions.deleteBook);
 
 
 
